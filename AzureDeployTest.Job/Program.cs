7d8759933fb6d1d5255b9bad0775aea6a355ddef
Harry McCarney <hmccarney@gmail.com>
2014-06-04 12:40:03 +0200

    added job

---
+++ AzureDeployTest.Job/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Loggly;
+using Serilog;
+
+namespace AzureDeployTest.Job
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+            while (true)
+            {
+                var logger = new Logger("72797290-0fb1-46cb-b33b-c45c191b6e5f");
+                logger.LogInfo("Hello World!");
+
+
+            }
+        }
+    }
+}
