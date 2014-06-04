4afcaab9b98b76ce97ba0d4fa71d5ca692ccf584
Harry McCarney <hmccarney@gmail.com>
2014-06-04 12:00:54 +0200

    testing git azure deploy

---
+++ AzureDeployTest.Api/HomeModule.cs
@@ -0,0 +1,13 @@
+﻿using Nancy;
+
+namespace AzureDeployTest.Api
+{
+    public class HomeModule :NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => "Hello world";
+
+        }
+    }
+}
